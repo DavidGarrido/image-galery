6225632b0e9a675d66bca2b06b2837340ecfc1d5
Davidagh <www.davidalexander@gmail.com>
2020-10-16 16:32:48 -0500

    Completando backend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        {{-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,12 +79,30 @@
                     @endauth
                 </div>
             @endif
-        </div>
+        </div> --}}
 
+        <form action=" {{route('image.store')}} " class="dropzone" id="my-awesome-dropzone" method="POST">
+        </form>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js" 
-                integrity="sha512-9WciDs0XP20sojTJ9E7mChDXy6pcO0qHpwbEJID1YVavz2H6QBz5eLoDD8lseZOb2yGT8xDNIV7HIe1ZbuiDWg==" 
-                crossorigin="anonymous">
-            </script>
+        {{-- <form action="{{route('image.store')}}" method="post" enctype="multipart/form-data">
+            @csrf
+            <input type="file" name="file" id="file">
+            <input type="submit" value="Enviar">
+        </form> --}}
+        
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js" integrity="sha512-9WciDs0XP20sojTJ9E7mChDXy6pcO0qHpwbEJID1YVavz2H6QBz5eLoDD8lseZOb2yGT8xDNIV7HIe1ZbuiDWg==" crossorigin="anonymous"></script>
+        
+        <script>
+            Dropzone.options.myAwesomeDropzone = {
+                headers:{
+                    'X-CSRF-TOKEN' : "{{csrf_token()}}",
+                },
+                acceptedFiles:'image/*,',
+                dictDefaultMessage: "Arrastra acá para cargar imagenes",
+                maxFilessize: 2,
+                maxFiles: 4,
+            };
+        </script>
     </body>
 </html>
